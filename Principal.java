a3290a98409bd1ed8acb96e0a8a4cb2b41654ff9
Miguel Angel Gtz Gomez <233382@ids.upchiapas.edu.mx>
2024-03-24 15:37:58 -0600

    [Update Restricciones]

--- Principal.java
+++ Principal.java
@@ -10,7 +10,7 @@ public class Principal {
         int eleccion;
         do{
             seleccionarPerfil();
-            System.out.println("Desea regresar /t 1.Si 2.No /t");
+            System.out.println("Desea regresar  1.Si\t 2.No \t");
             eleccion = entrada.nextInt();
         }while(eleccion == 1);
         
@@ -18,28 +18,34 @@ public class Principal {
 
     public static void seleccionarPerfil() {
         if (perfiles.isEmpty()) {
-            System.out.println("No hay perfiles, cree uno para poder manipular la información");
+            System.out.println("-----------No hay perfiles, cree uno para poder manipular la información-------------");
+            System.out.println("");
             crearNuevoPerfil();
         }
 
-        System.out.println("Bienvenido 1. Iniciar sesión 2. Crear nuevo perfil");
+        System.out.println("-----------------------------------Bienvenido-------------------------------------------");
+        System.out.println(" - ¿Que desea hacer?");
+        System.out.println("1.Iniciar sesión\t  2. Crear nuevo perfil\t (otra tecla). salir \t");
+        System.out.print("Opcion: ");
         eleccion = entrada.nextInt();
-
-        if (eleccion == 1) {
-            entrada.nextLine(); 
-
-            String nombreIngresado;
-            System.out.println("Ingrese el nombre del usuario");
-            nombreIngresado = entrada.nextLine();
-
-            for (Perfil perfil : perfiles) {
-                if (nombreIngresado.equals(perfil.getNombreUsuario())) {
-                    perfil.iniciarSesion();
-                    break; 
+        switch (eleccion) {
+            case 1:
+                String nombreIngresado;
+                entrada.nextLine();
+                System.out.println("Ingrese el nombre del usuario");
+                nombreIngresado = entrada.nextLine();
+                for (Perfil perfil : perfiles) {
+                    if (nombreIngresado.equals(perfil.getNombreUsuario())) {
+                        perfil.iniciarSesion();
+                        break; 
+                    }
                 }
-            }
-        } else {
-            crearNuevoPerfil();
+                break;
+            case 2:
+                crearNuevoPerfil();
+                break;
+            default:
+                break;
         }
     }
 
@@ -47,33 +53,47 @@ public class Principal {
         String nuevoNombreU, nuevaContrasenaU, aux;
         Perfil nuevoPerfil;
 
-        System.out.println("Qué tipo de perfil será: 1. Prefecto 2. Administrador");
-        eleccion = entrada.nextInt();
-        entrada.nextLine(); 
+        System.out.println(" - - ¿Qué tipo de perfil será?");
+    
+        int count = 0;
+        do{
+            if(count > 0){
+                System.out.println("Error ------ Seleccione por indice una de las siguientes opciones:");
+            }
+            System.out.println("\t1.Prefecto\t 2.Administrador\t ");
+            System.out.print("Opcion: ");
+            eleccion = entrada.nextInt();
+            entrada.nextLine(); 
+            count++;
+        }while(eleccion > 2 || eleccion < 1);
 
-        System.out.println("Ingrese las credenciales del nuevo perfil");
-        System.out.println("Ingrese el nuevo nombre de usuario");
+        System.out.println("Ingrese las credenciales del nuevo perfil empezando por: ");
+        System.out.print("Nombre de usuario: ");
         nuevoNombreU = entrada.nextLine();
 
         do {
-            System.out.println("Ingrese la nueva contraseña");
+            System.out.print("Contraseña: ");
             nuevaContrasenaU = entrada.nextLine();
 
-            System.out.println("Confirme la contraseña");
+            System.out.print("Confirme la contraseña:");
             aux = entrada.nextLine();
         } while (!aux.equals(nuevaContrasenaU));
         
 
         if (eleccion == 1) {
             nuevoPerfil = new PerfilPrefecto(nuevoNombreU, nuevaContrasenaU);
-            nuevoPerfil.setHorarios(perfiles.get(0).getHorarios());
+            if (perfiles.size() > 1){
+                nuevoPerfil.setHorarios(perfiles.get(0).getHorarios());
+            }
+            else{
+                System.out.println("Cree un usuario ADMINISTRADOR para cargar horarios y poder verlos en esta seccion.");
+            }
+            
         } else {
             nuevoPerfil = new PerfilAdministrador(nuevoNombreU, nuevaContrasenaU);
-
         }
 
         perfiles.add(nuevoPerfil);
-        seleccionarPerfil();
     }
 
     
